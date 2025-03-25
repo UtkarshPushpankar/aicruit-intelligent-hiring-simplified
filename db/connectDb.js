1030e584c1fc29dbd80391bd5aa9d92de0a5953c
UtkarshPushpankar <upushpankar@gmail.com>
2025-03-26 03:06:08 +0530

    sml chng

--- db/connectDb.js
+++ db/connectDb.js
@@ -1,15 +1,13 @@
 import mongoose from "mongoose";
 
 const connectDb = async () => {
-    try{
-        const conn = await mongoose.connect("mongodb://localhost:27017/aicruit", {
-            useNewUrlParser: true,
-        });
-        console.log(`MongoDB Connected: {conn.connection.host}`);
-    }catch(error){
-        console.error(error.message);
-        process.exit(1);
-    }
-}
+  try {
+    const conn = await mongoose.connect("mongodb://localhost:27017/aicruit");
+    console.log(`MongoDB Connected: ${conn.connection.host}`);
+  } catch (error) {
+    console.error(error.message);
+    process.exit(1);
+  }
+};
 
 export default connectDb;
