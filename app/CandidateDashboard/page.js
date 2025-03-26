954129273fb5ccb9e66d714c346d781e848e0535
UtkarshPushpankar <upushpankar@gmail.com>
2025-03-27 03:24:53 +0530

    Started Working on Candidate dashboard

--- app/CandidateDashboard/page.js
+++ app/CandidateDashboard/page.js
@@ -3,6 +3,7 @@ import React, { useEffect } from 'react';
 import { useSession, signOut } from "next-auth/react";
 import { useRouter } from 'next/navigation';
 import Slider from '@/components/Slider'
+import Search from "@/components/Search"
 
 const CandidateDashboard = () => {
   const { data: session } = useSession();
@@ -22,40 +23,29 @@ const CandidateDashboard = () => {
   const srcUserImage = session?.user?.image
 
   return (
-    <div className='candidate-nav flex gap-4 m-4'>
-      <div className="left w-[30vw] border-2 border-gray-400 rounded-lg h-[95.5vh] shadow-lg">
-        {/* <button onClick={() => signOut()} type="button" class="text-white bg-gradient-to-br from-purple-600 to-blue-500 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2">Logout</button> */}
-        <div className="profile m-6 mt-3 flex gap-5">
-          <img width={100} height={50} className='rounded-full' src={srcUserImage} alt="profile pic" />
-          <div className='flex flex-col gap-1 mt-1'>
-            <h2 className='font-bold text-xl poppins text-gray-600'>{session?.user?.name}</h2>
+    <div className="candidate-container flex h-screen">
+      <div className="sidebar w-[16vw] border-r-2 border-r-gray-300 bg-gradient-to-r from-[#398ab0] to-[#b7e0f3]">
+        <h1 className='m-4 p-2 text-center uncial text-4xl text-gradient-3'>
+          AIcruit
+        </h1>
+      </div>
 
-            <div className="info flex gap-6 text-gray-500">
-              <div className='flex gap-1 items-center'>
-                <img width={18} src="telephone.png" alt="" />
-                <p>7800181122</p>
-              </div>
+      <div className="main w-[84vw]">
+        <div className="top h-[10vh] border-b-2 border-b-gray-300 flex items-center justify-between">
+          <div className="search m-10">
+            <Search/>
+          </div>
 
-              <div className='flex gap-1 items-center'>
-                <img width={18} src="location.png" alt="" />
-                <p>Mumbai</p>
-              </div>
-            </div>
-
-            <div className="socials flex gap-5">
-              <img width={26} src="message.png" alt="" />
-              <img width={24} src="copy.png" alt="" />
-              <img width={24} src="share.png" alt="" />
+          <div className="right flex gap-8 items-center justify-center m-6">
+            <img className='cursor-pointer' width={25} src="notification.png" alt="" />
+            <img className='cursor-pointer' width={25} src="message.png" alt="" />
+            <div className="profile flex gap-4 items-center">
+              <img width={35} className='rounded-full cursor-pointer' src={srcUserImage} alt="" />
+              <div className='poppins font-semiboldbold'>{session?.user?.name}</div>
             </div>
           </div>
         </div>
-
-        <div className="slider w-[80%] m-auto">
-          <Slider />
-        </div>
       </div>
-      <div className="mid w-[45vw] border-2 border-gray-400 rounded-lg h-[95.5vh] shadow-lg"></div>
-      <div className="right w-[25vw] border-2 border-gray-400 rounded-lg h-[95.5vh] shadow-lg"></div>
     </div>
   );
 };
